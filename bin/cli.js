cb0ba4f02ff40b9270b5f8304933a0c727dd71e9
Kus Cámara <kcmr@users.noreply.github.com>
2019-02-24 17:58:54 +0100

    initial commit

---
+++ bin/cli.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const yargs = require('yargs');
+const {analyze} = require('../lib');
+
+const argv = yargs.argv;
+
+analyze([argv._[0]])
+  .then(result => console.log(result));
